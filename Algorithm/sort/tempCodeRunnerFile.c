be002793aa5025fa5635af50d0c553bfe7e9c66b
Emc2 <emc-ironh@qq.com>
2022-11-16 11:33:52 +0800

    删掉没用的代码

--- Algorithm/sort/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
- left; int right){
-//       if(left < right){
-//         int mid = left+right/2;
-//         mergeSort(A, left, mid);
-//         mergeSort(A, mid+1, right);
-//         merge(A, left, mid, mid+1, right);
-//       }
-// }
