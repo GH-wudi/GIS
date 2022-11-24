5832121661bdd6f4477f34f7b77d41c3f4cd732a
Emc2 <emc-ironh@qq.com>
2022-11-24 22:41:06 +0800

    增加顺序表结构体

---
+++ DataStructure/linear/basic/Sqlist.c
@@ -0,0 +1,24 @@
+/**
+ * C 语言: 顺序表及其操作
+ *
+ * @author wfh
+ * @date 2022/11/11
+ */
+
+#include <stdio.h>
+ // #include <stdlib.h>
+#define MAXSIZE 100
+
+typedef struct Sqlist
+{
+	int data[MAXSIZE];
+	int length;
+}Sqlist;
+
+int main()
+{
+	int num;
+	scanf("%d", num);
+	printf("Hello, world!\n%d", num);
+	return 0;
+}
